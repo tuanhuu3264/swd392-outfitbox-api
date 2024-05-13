30a678cd4fe69b2273df21192aca898e19ad7363
Vu Hoang Duy Khanh <khanh911234@gmail.com>
2024-05-13 19:54:03 +0700

    Seed data Brand,Type,Category

---
+++ SWD392.OutfitBox.Domain/Entities/Brand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWD392.OutfitBox.Domain.Entities
+{
+    [Table("Brand")]
+    public class Brand
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Status {  get; set; }
+        public string Link { get; set; }
+    }
+}
---
+++ SWD392.OutfitBox.Infratures/Repositories/IBaseRepository.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq.Expressions;
+
+namespace FAMS.Api.Repositories
+{
+    public interface IBaseRepository<TEntity> where TEntity : class
+    {
+        IQueryable<TEntity> Get();
+
+        Task<TEntity?> GetById(object id, CancellationToken cancellationToken = default);
+
+        Task AddRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default);
+
+        Task<TEntity> AddAsync(TEntity entity, CancellationToken cancellationToken = default);
+
+        void Delete(params TEntity[] entities);
+
+        void Update(params TEntity[] entities);
+
+        void Delete(TEntity entity);
+
+        void Update(TEntity entity);
+
+        Task SaveChangesAsync(CancellationToken cancellationToken = default);
+
+        public Task<IEnumerable<TEntity>?> Find(Expression<Func<TEntity, bool>>? filter = null,
+            string includeProperties = "",
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null);
+
+        public Task<TEntity?> FindOne(Expression<Func<TEntity, bool>>? filter = null, string includeProperties = "");
+
+        public Task<bool> AddHashKey(TEntity entity, CancellationToken cancellationToken = default);
+    }
+}
