879fd57dd8fad8527763fce31cf973fa61114368
Vu Hoang Duy Khanh <khanh911234@gmail.com>
2024-05-14 00:08:58 +0700

    Product service,product controller

---
+++ SWD392.OutfitBox.API/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using SWD392.OutfitBox.API.Controllers.Endpoints;
+using SWD392.OutfitBox.Core.Services.ProductService;
+
+namespace SWD392.OutfitBox.API.Controllers
+{
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        readonly IProductService _productService;
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        [HttpGet(Endpoints.ProductsController.getAllProducts)]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _productService.GetAll();
+            return StatusCode((int)result.StatusCode, result.Data);
+        }
+    }
+}
